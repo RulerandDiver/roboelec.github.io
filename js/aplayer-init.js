7aedae6bc34daa7ba2f264cbfac57f7470ca8a6f
RulerandDiver <1569256816@qq.com>
2026-03-07 11:05:36 +0800

    Site updated: 2026-03-07 11:05:36

--- js/aplayer-init.js
+++ js/aplayer-init.js
@@ -12,28 +12,10 @@ document.addEventListener('DOMContentLoaded', function () {
     lrcType: 0,
     audio: [
       {
-        name: 'Bensound - Sunny',
-        artist: 'Bensound',
-        url: 'https://www.bensound.com/bensound-music/bensound-sunny.mp3',
-        cover: 'https://images.unsplash.com/photo-1506905925346-21bda4d32df4?w=300&q=80'
-      },
-      {
-        name: 'Bensound - Ukulele',
-        artist: 'Bensound',
-        url: 'https://www.bensound.com/bensound-music/bensound-ukulele.mp3',
-        cover: 'https://images.unsplash.com/photo-1518709268805-4e9042af9f23?w=300&q=80'
-      },
-      {
-        name: 'Bensound - Creative Minds',
-        artist: 'Bensound',
-        url: 'https://www.bensound.com/bensound-music/bensound-creativeminds.mp3',
-        cover: 'https://images.unsplash.com/photo-1511447333015-45b65e60f6d5?w=300&q=80'
-      },
-      {
-        name: 'Bensound - Acoustic Breeze',
-        artist: 'Bensound',
-        url: 'https://www.bensound.com/bensound-music/bensound-acousticbreeze.mp3',
-        cover: 'https://images.unsplash.com/photo-1470225620780-dba8ba36b745?w=300&q=80'
+        name: '反方向的钟',
+        artist: '周杰伦',
+        url: '/roboelec.github.io/music/未知歌手 - 反方向的钟.mp3',
+        cover: 'https://images.unsplash.com/photo-1511379938547-c1f69419868d?w=300&q=80'
       }
     ]
   });
